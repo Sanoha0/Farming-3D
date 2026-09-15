0e2c135c97af14ca150e7ddbe92ea11f22b844a6
Samoha <159712838+Sanoha0@users.noreply.github.com>
2026-09-14 23:32:07 -0500

    Update game target build settings for UE5.8

--- Farming3D/Source/Farming3D.Target.cs
+++ Farming3D/Source/Farming3D.Target.cs
@@ -6,6 +6,7 @@ public class Farming3DTarget : TargetRules
     public Farming3DTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         ExtraModuleNames.Add("Farming3D");
     }
 }
