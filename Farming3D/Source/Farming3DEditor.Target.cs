e6836bfb2116592550ee0c01adcb9ff72f4de0ad
Samoha <159712838+Sanoha0@users.noreply.github.com>
2026-09-14 23:32:02 -0500

    Update editor target build settings for UE5.8

--- Farming3D/Source/Farming3DEditor.Target.cs
+++ Farming3D/Source/Farming3DEditor.Target.cs
@@ -6,6 +6,7 @@ public class Farming3DEditorTarget : TargetRules
     public Farming3DEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         ExtraModuleNames.Add("Farming3D");
     }
 }
